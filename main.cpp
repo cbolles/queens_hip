b1020467534742b5632ec168abe5aaf2fc9afbbc
Collin Bolles <collinbolles@gmail.com>
2020-11-05 00:26:55 -0500

    Working ability to get fitness

--- main.cpp
+++ main.cpp
@@ -1,17 +1,33 @@
 #include <cstdint>
+#include <iostream>
 #include <stdlib.h>
 #include <time.h>
-
+#include <bits/stdc++.h>
 #include <hip/hip_runtime.h>
 
 #define BLOCK_SIZE 512
 #define THREADS_PER_BLOCK 256
 #define BOARD_SIZE 8
 
+using namespace std;
+
 typedef struct individual_s {
     uint16_t fitness;
     uint8_t queensPosition[BOARD_SIZE];
 } individual;
+
+
+// Copied from HIP bit_extrack sample
+#define CHECK(cmd)                                                                                 \
+    {                                                                                              \
+        hipError_t error = cmd;                                                                    \
+        if (error != hipSuccess) {                                                                 \
+            fprintf(stderr, "error: '%s'(%d) at %s:%d\n", hipGetErrorString(error), error,         \
+                    __FILE__, __LINE__);                                                           \
+            exit(EXIT_FAILURE);                                                                    \
+        }                                                                                          \
+    }
+
 
 /**
  * Handles initializing the population with random locations for the queens. 
@@ -24,6 +40,7 @@ void initPopulation(individual *population, uint16_t populationSize) {
         for(uint8_t rowIndex = 0; rowIndex < BOARD_SIZE; rowIndex++) {
             // A queen can show up anywhere on the row from index 0 up to the size of the board
             uint8_t randomQueenIndex = rand() % BOARD_SIZE;
+            cout << randomQueenIndex << endl;
             population[individualIndex].queensPosition[rowIndex] = randomQueenIndex;
             population[individualIndex].fitness = 0;
         }
@@ -42,6 +59,7 @@ __global__
 void calculateFitness(individual *population, uint16_t populationSize) {
     // Determine the individual to calculate the fitness for
     size_t individualIndex = hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x;
+    
 
     // Make sure we are within the bounds of the population count
     if(individualIndex >= populationSize) { return; }
@@ -51,12 +69,12 @@ void calculateFitness(individual *population, uint16_t populationSize) {
     uint16_t fitness = ((BOARD_SIZE - 1) * (BOARD_SIZE - 2)) / 2;
     #pragma unroll
     for(int rowIndex = 0; rowIndex < BOARD_SIZE; rowIndex++) {
-        uint8_t queenPosition = ind.queensPosition[rowIndex];
+        uint8_t queenPosition = population[individualIndex].queensPosition[rowIndex];
 
         // Check each other queen location and check for a collision
         #pragma unroll
         for(int positionCheck = rowIndex + 1; positionCheck < BOARD_SIZE - 1; positionCheck++) {
-            uint8_t otherQueenPosition = ind.queensPosition[positionCheck];
+            uint8_t otherQueenPosition = population[individualIndex].queensPosition[positionCheck];
 
             // If the queens are in the same column, that is a collision
             if(queenPosition == otherQueenPosition) {
@@ -68,36 +86,51 @@ void calculateFitness(individual *population, uint16_t populationSize) {
                 fitness--;
             }
         }
-        // Update fitness
-        ind.fitness = fitness;
     }
+    // Update fitness
+    population[individualIndex].fitness = fitness;
+}
+
+/**
+ * Used to compare and sort individuals in decenting order based on fitness
+ */
+bool compareIndividuals(individual first, individual second) {
+    return first.fitness > second.fitness;
 }
 
 int main() {
+    cout << "Being N-Queens Solver" << endl;
+
     // Size of the population during each generation
     uint16_t populationSize = 100;
     // Maximum number of generations to go through before ending
     uint16_t maxGenerations = 200;
+
+    const uint16_t targetFitness = ((BOARD_SIZE - 1) * (BOARD_SIZE - 2)) / 2;
 
     // Initialize randomization
     srand(time(NULL));
 
     // Initialize the popultation with initially random data
     individual *h_population = static_cast<individual*>(calloc(populationSize, sizeof(individual)));
+    initPopulation(h_population, populationSize);
 
     // Initialize device variables
     individual *d_population;
-    hipMalloc(&d_population, populationSize * sizeof(individual));
+    CHECK(hipMalloc(&d_population, populationSize * sizeof(individual)));
 
     for(uint16_t populationId = 0; populationId < maxGenerations; populationId++) {
         // Calculate the fitness of the population
-        hipMemcpy(d_population, h_population, populationSize * sizeof(individual), hipMemcpyHostToDevice);
+        CHECK(hipMemcpy(d_population, h_population, populationSize * sizeof(individual), hipMemcpyHostToDevice));
         hipLaunchKernelGGL(calculateFitness, dim3(BLOCK_SIZE), dim3(THREADS_PER_BLOCK), 0, 0, d_population, populationSize);
-         
-        // Sort the population based on fitness
+        CHECK(hipMemcpy(h_population, d_population, populationSize * sizeof(individual), hipMemcpyDeviceToHost));
 
-        
+        // Sort the population based on fitness
+        sort(h_population, h_population + populationSize, compareIndividuals);
+
         // Display the current highest fitness
+        cout << h_population[0].fitness << endl;
+        //cout << h_population[0].queensPosition[0] << endl;
 
         // Check to see if the end condition has been reached
 
